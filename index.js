e4249905f409cc3a8a54927b497727c25f2014d8
Diamond-FoxUA <dimofarb@gmail.com>
2025-09-27 15:30:01 +0000

    Deploying to gh-pages from @ Diamond-FoxUA/goit-js-hw-12@3e1ed1e74b36ad5ad139eceeb2c4236c5df9fb89 🚀

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-import{a as E,S as k,i as h}from"./assets/vendor-BNibzuFn.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const i of t)if(i.type==="childList")for(const l of i.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&n(l)}).observe(document,{childList:!0,subtree:!0});function o(t){const i={};return t.integrity&&(i.integrity=t.integrity),t.referrerPolicy&&(i.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?i.credentials="include":t.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function n(t){if(t.ep)return;t.ep=!0;const i=o(t);fetch(t.href,i)}})();async function p(e,s=1){try{return(await E("https://pixabay.com/api/",{params:{key:"52353734-aa83943fc4859b94495f66918",q:e,page:s,per_page:15,image_type:"photo",orientation:"horizontal",safesearch:!0}})).data}catch(o){return console.log(o),{hits:[],totalHits:0}}}let m;function y(e,s){const o=e.map(({webformatURL:n,largeImageURL:t,tags:i,likes:l,views:w,comments:S,downloads:q})=>`
+import{a as E,S as H,i as y}from"./assets/vendor-BNibzuFn.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const i of document.querySelectorAll('link[rel="modulepreload"]'))n(i);new MutationObserver(i=>{for(const s of i)if(s.type==="childList")for(const a of s.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function r(i){const s={};return i.integrity&&(s.integrity=i.integrity),i.referrerPolicy&&(s.referrerPolicy=i.referrerPolicy),i.crossOrigin==="use-credentials"?s.credentials="include":i.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function n(i){if(i.ep)return;i.ep=!0;const s=r(i);fetch(i.href,s)}})();async function g(e,t=1){try{return(await E("https://pixabay.com/api/",{params:{key:"52353734-aa83943fc4859b94495f66918",q:e,page:t,per_page:15,image_type:"photo",orientation:"horizontal",safesearch:!0}})).data}catch(r){return console.log(r),{hits:[],totalHits:0}}}let f;function L(e,t){const r=e.map(({webformatURL:n,largeImageURL:i,tags:s,likes:a,views:P,comments:q,downloads:k})=>`
     <li class="card">
-      <a href="${t}" class="card-item-img">
-        <img class="img" src="${n}" alt="${i}">
+      <a href="${i}" class="card-item-img">
+        <img class="img" src="${n}" alt="${s}">
       </a>
       <div class="card-description">
         <ul class="desc-item">
           <li class="desk-item-name">Likes</li>
-          <li class="desk-item-value">${l}</li>
+          <li class="desk-item-value">${a}</li>
         </ul>
         <ul class="desc-item">
           <li class="desk-item-name">Views</li>
-          <li class="desk-item-value">${w}</li>
+          <li class="desk-item-value">${P}</li>
         </ul>
         <ul class="desc-item">
           <li class="desk-item-name">Comments</li>
-          <li class="desk-item-value">${S}</li>
+          <li class="desk-item-value">${q}</li>
         </ul>
         <ul class="desc-item">
           <li class="desk-item-name">Downloads</li>
-          <li class="desk-item-value">${q}</li>
+          <li class="desk-item-value">${k}</li>
         </ul>
       </div>
     </li>
-  `).join("");s.insertAdjacentHTML("beforeend",o),m?m.refresh():m=new k(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"})}function $(e){e.innerHTML=""}function g(e){e.classList.remove("hidden")}function L(e){e.classList.add("hidden")}function b(e){e.classList.remove("hidden")}function f(e){e.classList.add("hidden")}const v=document.querySelector(".form"),B=v.querySelector("input");document.querySelector(".form-btn");const a=document.querySelector(".gallery"),r=document.querySelector(".more-btn"),d=document.querySelector(".loader");let u=1,c="";v.addEventListener("submit",async e=>{if(e.preventDefault(),c=B.value.trim(),!c)return;u=1,$(a),f(r),g(d);const{hits:s,totalHits:o}=await p(c,u);if(L(d),s.length===0){h.error({message:"some error",position:"topRight"});return}y(s,a),o>s.length&&b(r)});r.addEventListener("click",async()=>{u++,r.disabled=!0,f(r),g(d);const{hits:e,totalHits:s}=await p(c,u);L(d),b(r),y(e,a),a.children.length>=s&&(f(r),h.info({message:"We're sorry, but you've reached the end of search results.",position:"topRight"})),r.disabled=!1,H()});function H(){const{height:e}=a.firstElementChild.getBoundingClientRect();window.scrollBy({top:e*2,behavior:"smooth"})}
+  `).join("");t.insertAdjacentHTML("beforeend",r),f?f.refresh():f=new H(".gallery a",{captionsData:"alt",captionDelay:250,captionPosition:"bottom"}),t.children.length===1&&(t.children[0].style.width="360px")}function $(e){e.innerHTML=""}function w(e){e.classList.remove("hidden")}function b(e){e.classList.add("hidden")}function M(e){e.classList.remove("hidden")}function h(e){e.classList.add("hidden")}const v=document.querySelector(".form"),O=v.querySelector("input"),l=document.querySelector(".gallery"),o=document.querySelector(".more-btn"),u=document.querySelector(".loader");let m=1,c="",p=0;v.addEventListener("submit",async e=>{if(e.preventDefault(),c=O.value.trim(),!c){d("Please fill out the search field.");return}m=1,$(l),h(o),w(u);try{const{hits:t,totalHits:r}=await g(c,m);if(p=r,!t.length){d("Sorry, no images found. Please try a different search query.");return}L(t,l),S(l,p,o)}catch(t){d("Something went wrong. Please try again later."),console.error(t)}finally{b(u)}});o.addEventListener("click",async()=>{m++,o.disabled=!0,h(o),w(u);try{const{hits:e,totalHits:t}=await g(c,m);L(e,l),x(),S(l,t,o)}catch(e){d("Something went wrong. Please try again later."),console.log(e)}finally{b(u),o.disabled=!1}});function x(){const{height:e}=l.firstElementChild.getBoundingClientRect();window.scrollBy({top:e*2,behavior:"smooth"})}function d(e){y.error({message:e,position:"topRight"})}function S(e,t,r){e.children.length>=t?(h(r),y.info({message:"We're sorry, but you've reached the end of search results.",position:"topRight"})):M(r)}
 //# sourceMappingURL=index.js.map
